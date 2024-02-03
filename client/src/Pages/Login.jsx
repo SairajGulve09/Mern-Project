ae03f390808377f901d59df0b330c24bf9d1d4c0
Sairaj Gulve <130494632+SairajGulve09@users.noreply.github.com>
2024-02-03 23:51:53 +0530

    Update Login.jsx

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch(`http://localhost:4000/api/auth/login`, {
+            const response = await fetch(`https://mern-website-694a.onrender.com/api/auth/login`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
