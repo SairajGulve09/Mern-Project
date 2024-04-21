f887032bbc44e9af4ee5862709e77c5fda2ebc88
Sairaj Gulve <130494632+SairajGulve09@users.noreply.github.com>
2024-04-21 16:58:32 +0530

    Update Register.jsx

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -33,7 +33,7 @@ const Register = () =>{
         console.log(user);
 
         try {
-            const response = await fetch(`http://localhost:4000/api/auth/register`,{
+            const response = await fetch(`https://polyverse-oo8c7ej1x-sairajgulve09s-projects.vercel.app/register`,{
             method: "POST",
             headers:{
                 "Content-Type":"application/json",
